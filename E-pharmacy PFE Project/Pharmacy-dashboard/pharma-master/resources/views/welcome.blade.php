d468d641ef5ee564a6de2ea06eca06b76ef7f797
ElhaddajAya <ayaelhaddaj619@gmail.com>
2024-03-13 14:33:59 +0000

    Updated welcom.blade 2

--- E-pharmacy PFE Project/Pharmacy-dashboard/pharma-master/resources/views/welcome.blade.php
+++ E-pharmacy PFE Project/Pharmacy-dashboard/pharma-master/resources/views/welcome.blade.php
@@ -333,44 +333,7 @@
         </div>
       </div>
     </div>
-
-    {{-- <div class="card mb-6">
-      <header class="card-header">
-        <p class="card-header-title">
-          <span class="icon"><i class="mdi mdi-finance"></i></span>
-          Performance
-        </p>
-        <a href="#" class="card-header-icon">
-          <span class="icon"><i class="mdi mdi-reload"></i></span>
-        </a>
-      </header>
-      <div class="card-content">
-        <div class="chart-area">
-          <div class="h-full">
-            <div class="chartjs-size-monitor">
-              <div class="chartjs-size-monitor-expand">
-                <div></div>
-              </div>
-              <div class="chartjs-size-monitor-shrink">
-                <div></div>
-              </div>
-            </div>
-            <canvas id="big-line-chart" width="2992" height="1000" class="chartjs-render-monitor block" style="height: 400px; width: 1197px;"></canvas>
-          </div>
-        </div>
-      </div>
-    </div> --}}
-
-    {{-- <div class="notification blue">
-      <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0">
-        <div>
-          <span class="icon"><i class="mdi mdi-buffer"></i></span>
-          <b>Responsive table</b>
-        </div>
-        <button type="button" class="button small textual --jb-notification-dismiss">Dismiss</button>
-      </div>
-    </div> --}}
-
+    
     <div class="card has-table">
       <header class="card-header">
         <p class="card-header-title">
