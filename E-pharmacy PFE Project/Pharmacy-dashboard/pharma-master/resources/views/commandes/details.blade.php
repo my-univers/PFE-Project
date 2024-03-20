7631e5eb6cfa9fdf82ffdc1400aa022cca3d2d60
ElhaddajAya <ayaelhaddaj619@gmail.com>
2024-03-20 13:36:55 +0000

    Updated commandes.details sidebar

--- E-pharmacy PFE Project/Pharmacy-dashboard/pharma-master/resources/views/commandes/details.blade.php
+++ E-pharmacy PFE Project/Pharmacy-dashboard/pharma-master/resources/views/commandes/details.blade.php
@@ -92,7 +92,7 @@
           </ul>
         </li>
       <li>
-      <li>
+      {{-- <li>
         <a class="dropdown">
           <span class="icon"><i class="mdi mdi-pill"></i></span>
           <span class="menu-item-label">Médicaments</span>
@@ -148,7 +148,7 @@
             </a>
           </li>
         </ul>
-      </li>
+      </li> --}}
       <li class="active">
         <a class="dropdown">
           <span class="icon"><i class="mdi mdi-cart"></i></span>
@@ -187,7 +187,7 @@
           </li>
         </ul>
       </li>
-      <li>
+      {{-- <li>
         <a class="dropdown">
           <span class="icon"><i class="mdi mdi-medical-bag"></i></span>
           <span class="menu-item-label">Packs Premiers Secours</span>
@@ -205,7 +205,7 @@
             </a>
           </li>
         </ul>
-      </li>
+      </li> --}}
     </ul>
     <p class="menu-label">A Propos</p>
     <ul class="menu-list">
