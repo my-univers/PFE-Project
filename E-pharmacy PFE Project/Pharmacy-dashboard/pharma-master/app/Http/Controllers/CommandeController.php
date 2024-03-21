6fc9fc35635292ecf359a0e70ff6beb067fb1e11
ElhaddajAya <ayaelhaddaj619@gmail.com>
2024-03-21 15:47:13 +0000

    Added pagination to clients in commandes add form

--- E-pharmacy PFE Project/Pharmacy-dashboard/pharma-master/app/Http/Controllers/CommandeController.php
+++ E-pharmacy PFE Project/Pharmacy-dashboard/pharma-master/app/Http/Controllers/CommandeController.php
@@ -37,8 +37,8 @@ class CommandeController extends Controller
     }
 
     public function addCommandeForm() {
-        $clients = Client::all();
-        $produits = Produit::all();
+        $clients = Client::paginate(5);
+        $produits = Produit::paginate(5);
 
         return view('commandes.add', ['clients' => $clients, 'produits' => $produits]);
     }
