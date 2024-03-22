c3ab36b8c35abbf6d84348e3105b50da9a1e4f18
ElhaddajAya <ayaelhaddaj619@gmail.com>
2024-03-22 17:00:13 +0000

    Added packs commandes

---
+++ E-pharmacy PFE Project/Pharmacy-dashboard/pharma-master/database/migrations/2024_03_22_161348_create_packs_commandes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('packs_commandes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('commande_id')->constrained('commandes')->onDelete('cascade');
+            $table->foreignId('pack_id')->constrained('packs')->onDelete('cascade');
+            $table->integer('quantite')->nullable(false);  // quantité du produit commandé
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('packs_commandes');
+    }
+};
