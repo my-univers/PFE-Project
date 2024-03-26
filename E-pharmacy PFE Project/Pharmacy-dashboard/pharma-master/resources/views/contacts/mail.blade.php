469f1fd5a0706549c06231532ff924b8133af01e
my-univers <flaarif141@gmail.com>
2024-03-26 17:15:11 +0000

    mail

--- E-pharmacy PFE Project/Pharmacy-dashboard/pharma-master/resources/views/contacts/mail.blade.php
+++ E-pharmacy PFE Project/Pharmacy-dashboard/pharma-master/resources/views/contacts/mail.blade.php
@@ -11,49 +11,8 @@
 </head>
 
 <body>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            background-color: #f4f4f4;
-        }
-        .container {
-            max-width: 600px;
-            margin: 0 auto;
-            padding: 20px;
-            background-color: #fff;
-            border-radius: 8px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        }
-        p {
-            color: #666;
-            line-height: 1.6;
-        }
-
-        .button {
-            display: inline-block;
-            padding: 10px 20px;
-            background-color: #007bff;
-            color: #fff;
-            text-decoration: none;
-            border-radius: 4px;
-        }
-
-        .button:hover {
-            background-color: #0056b3;
-        }
-
-    </style>
-
-
-<div class="container">
     <p>Bonjour {{$data['nom']}}</p>
     <p>{{ $data['body'] }}</p>
-</div>
-
-<p style="text-align: center;"><a href="#" class="btn btn-primary">Visitez notre site web</a></p>
-
 </body>
 
 </html>
